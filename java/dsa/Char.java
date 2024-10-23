60dd22ca7b01094ed0f35a412ab7f728b0ac15b4
Varadharajan <116930167+V-Varadharajan@users.noreply.github.com>
2024-10-23 16:15:56 +0530

    Updated

--- java/dsa/Char.java
+++ java/dsa/Char.java
@@ -6,9 +6,9 @@ class Char {
     }
 
     static int Export(char[] arr) {
-        int max = 0;
+        int max = arr[0];
         for(int i =0;i<arr.length; i++) {
-            if(i < arr[max]) {
+            if(max < arr[i]) {
                 max = i;
             }
         }
